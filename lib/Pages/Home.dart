8e5d44da2641230c0bd59dd237e47f652947d459
Hayder Ali <hyderali0889@gmail.com>
2022-03-30 21:11:53 +0500

    added args and obs , obx

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,42 +11,46 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var count = 0.obs;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        TextButton(
-          child: const Text("Get SnackBar"),
-          onPressed: snackbar,
-        ),
+        Obx(() => TextButton(
+              child: Text("$count"),
+              onPressed: () {
+                count++;
+              },
+            )),
         TextButton(
           child: const Text("Go To Page two"),
           onPressed: () {
-            Get.toNamed("/second");
+            Get.toNamed("/second" , arguments: "Ali");
           },
         ),
       ],
     );
   }
 
-  void snackbar() {
-    // Get.snackbar("Example", "Dont Touch ME",
-    //     backgroundColor: Colors.red,
-    //     duration: const Duration(seconds: 3),
-    //     borderRadius: 14.0,
-    //     colorText: Colors.white,
-    //     snackPosition: SnackPosition.BOTTOM);
+  // void snackbar() {
+  //   // Get.snackbar("Example", "Dont Touch ME",
+  //   //     backgroundColor: Colors.red,
+  //   //     duration: const Duration(seconds: 3),
+  //   //     borderRadius: 14.0,
+  //   //     colorText: Colors.white,
+  //   //     snackPosition: SnackPosition.BOTTOM);
 
-    Get.dialog(AlertDialog(
-      title:const Text("Book You"),
-      actions: [
-        TextButton(
-            onPressed: () {
-              Get.back();
-            },
-            child: const Text("X"))
-      ],
-    ));
-  }
+  //   Get.dialog(AlertDialog(
+  //     title:const Text("Book You"),
+  //     actions: [
+  //       TextButton(
+  //           onPressed: () {
+  //             Get.back();
+  //           },
+  //           child: const Text("X"))
+  //     ],
+  //   ));
+  // }
 }
--- lib/Pages/Second.dart
+++ lib/Pages/Second.dart
@@ -1,18 +1,21 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: file_names
 
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Second extends StatefulWidget {
-  const Second({ Key? key }) : super(key: key);
+  const Second({Key? key}) : super(key: key);
 
   @override
   State<Second> createState() => _SecondState();
 }
 
 class _SecondState extends State<Second> {
+  var args = Get.arguments;
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Welcome to Page two"),
+    return Center(
+      child: Text("Welcome to Page two $args"),
     );
   }
 }
